bb513a1431a279e8aa3d233d4c7f102d37976860
kdddru <dhk9817@gmail.com>
2023-03-02 13:19:48 +0900

    0302

---
+++ JS/js/0302 변수.js
@@ -0,0 +1,18 @@
+let num1 =10;
+
+console.log(num1+10);
+
+/*
+let name = "성춘향";
+let age = 20;
+let num = "010-123-4567";
+*/
+
+const user = {
+    name : "성춘향",
+    age : 20,
+    number : "010-123-4567"
+}
+
+console.log(user);
+
---
+++ JS/js/0302.js
@@ -0,0 +1,11 @@
+console.log("hello js");
+//아무것도 뜨지 않네용
+console.log(5+2);
+console.log("반갑습니다\n홍길동입니다");
+console.log(`100*273+23=${100*273+23}`);
+
+console.log(3 != 3);
+console.log(3+2 == 5);
+console.log("hi" != "hello");
+console.log(true||false);
+
