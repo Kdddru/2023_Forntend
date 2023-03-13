8676e76809ee7a654ee05095878f4b3e9c6c0c78
Kdddru <125441996+Kdddru@users.noreply.github.com>
2023-03-13 21:37:57 +0900

    0313

---
+++ 0311 객체/js/1.js
@@ -0,0 +1,6 @@
+function numCheck(){
+    let randomNum1 = parseInt(Math.random() * 10);
+    let randomNum2 = parseInt(Math.random() * 10);
+    let randomNum3 = parseInt(Math.random() * 10);
+    document.write(`${randomNum1}${randomNum2}${randomNum3}`);
+}
