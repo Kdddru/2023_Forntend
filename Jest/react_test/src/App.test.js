bd554e4a60a20fabd172654fc430121a42e28f08
Kdddru <dhk9817@gmail.com>
2023-07-13 12:07:01 +0900

    0713 App test

--- Jest/react_test/src/App.test.js
+++ Jest/react_test/src/App.test.js
@@ -4,11 +4,31 @@
 // >> render를 통해서 가져와 쓸 수 도 있다
 //ex) const {getByText} = render(컴포넌트)
 
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   // const linkElement = screen.getByText(/메롱/i);
   //expect(linkElement).toBeInTheDocument();
+
+
+  const view = screen.getByText('현재 숫자 : 0');
+
+  expect(view).toBeInTheDocument();
+
+
 });
+
+it('App에서 증가버튼을 눌렀을때 값이 바뀌는지', ()=>{
+  render(<App/>)
+
+  const incrementBtn = screen.getByTestId('incrementbtn');
+
+  fireEvent.click(incrementBtn);
+
+  const changeState = screen.getByText('현재 숫자 : 1');
+  expect(changeState).toBeInTheDocument();
+
+
+})
